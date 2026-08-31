bc5ceadef3147f3e5ff5d4158c15cd6d996517b1
ENOCK BETT <protichenoch1@gmail.com>
2026-08-31 20:14:35 +0300

    Create FaqsPage component with FaqSection

---
+++ app/faqs/page.js
@@ -0,0 +1,9 @@
+import FaqSection from '@/components/FaqSection';
+
+export default function FaqsPage() {
+  return (
+    <div className="max-w-4xl mx-auto p-4 md:p-6 space-y-6">
+      <FaqSection />
+    </div>
+  );
+}
