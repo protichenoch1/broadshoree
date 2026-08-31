512ae0af88d242545a108d8e8ad08e880aa985c1
ENOCK BETT <protichenoch1@gmail.com>
2026-08-31 20:13:43 +0300

    Add AboutPage component with AboutSection

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+import AboutSection from '@/components/AboutSection';
+
+export default function AboutPage() {
+  return (
+    <div className="max-w-4xl mx-auto p-4 md:p-6 space-y-6">
+      <AboutSection />
+    </div>
+  );
+}
